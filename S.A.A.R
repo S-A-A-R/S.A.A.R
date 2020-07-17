94554a69340500ec570d6377e242585dc43057f4
ikramuddin1234 <68438827+ikramuddin1234@users.noreply.github.com>
2020-07-17 20:30:36 +0300

    Add files via upload

---
+++ S.A.A.R
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>S.A.A.R</title>
+        <style>
+       
+      #a {
+        font-family:"Arial", sans-serif;
+         font-size: 2.2em;
+        }
+       
+       #b {
+        font-family:"Arial", sans-serif;
+         font-size: 1.8em;
+        }
+        
+        #c {
+        font-family:"Arial", sans-serif;
+         font-size: 1.5em;
+        }
+        
+         #d {
+        font-family:"Arial", sans-serif;
+         font-size: 1.5em;
+        }
+        
+         #e  {
+        font-family:"Arial", sans-serif;
+         font-size: 1.5em;
+        }
+        
+         #f {
+        font-family:"Arial", sans-serif;
+         font-size: 1.3em;
+        }
+         #g {
+        font-family:"Arial", sans-serif;
+         font-size: 1.2em;
+        }
+        
+        
+        #idc {
+        font-family:"Arial", sans-serif;
+         font-size: 3.9em;
+        }
+        </style>
+    </head>
+    <body>
+
+    <h1 id="idc">S.A.A.R</h1>
+    
+    <h2 id="a">Saudi.Arabia.Animal.Rescue</h2>
+    
+    <p id="c">This is a NGO created by Ikramuddin Mohammed with the help of his friends to save the abandoned cats.<strong>We will help other animals when the NGO gets bigger.</strong>In Saudi  Arabia with our help we can save countless lives of animals </p>
+    
+    <p id="d">The NGO is not registered and will be registered after first meeting with the members.You can become a member  if you agree to the terms and conditions.</p>
+
+      
+    <h3 id="e">Terms and Condition</h3>
+    <ol id="f">
+        <li>You <strong> have </strong> to atttend a meeting without fail (if <strong> important </strong> message me on zoom chat in the meeting)</li>
+        <li>You will be given a job according to your skill set          </li>
+        <li>You may or may not be funded for your job</li>
+        <li>Some people  may have to travel <strong>(not far) </strong></li>
+    </ol>
+    
+    <p id="g">click the link below if you want to be officialy a member<br><a href="https://docs.google.com/forms/d/e/1FAIpQLSc5I_x6EHdjlTdNPc60KzFnsLCwM8D5oIImi3c5lKmsqog1ZA/viewform">https://docs.google.com/forms/d/e/1FAIpQLSc5I_x6EHdijlTdnPc60KzFnsLCwM8D5ollmi3c5Imsqog1ZA/viewform</a>.</p>
+    
+    </body>
+</html>
